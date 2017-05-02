1dc12378054b8675dcffd16c3b68f8a14ac21848
Hari Pilla <Hari.Pilla@bcbsnc.com>
2017-05-02 13:38:56 -0400

    v5

---
+++ AddressBook.Tests/RooldexTests.cs
@@ -0,0 +1,74 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+
+
+namespace AddressBook.Tests
+{
+    [TestFixture]
+    public class RooldexTests
+    {
+        private IGetInputFromUsers _input;
+        private IHandleContacts _contacts;
+        private IHandleRecipes _recipes;
+        private Rolodex _rolodex;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _input      = Substitute.For<IGetInputFromUsers>();
+            _contacts   = Substitute.For<IHandleContacts>();
+            _recipes    = Substitute.For<IHandleRecipes>();
+            _rolodex    = new Rolodex(_contacts,_recipes,_input);
+        }
+        [Test]
+        public void ExitJustDoesNothing()
+        {
+            //Arrange 
+            _input.GetNumber().Returns(0);
+
+            //Act
+            _rolodex.DoStuff();
+            //Assert
+            _input.Received().GetNumber();
+            _contacts.DidNotReceive().GetAllContacts();
+            _recipes.DidNotReceive().GetAllRecipes();
+            _contacts.DidNotReceive().CreateCompany(null,null);
+        }
+        [Test]
+        public void AddPerson()
+        {
+            //Arrange 
+
+            _input.GetNumber().Returns(1,0);
+            _input.GetNonEmptyString().Returns("Bob", "Marley","555-555-9999");
+            
+            //Act
+            _rolodex.DoStuff();
+            //Assert
+            _input.Received(2).GetNumber();
+            _contacts.DidNotReceive().GetAllContacts();
+            _recipes.DidNotReceive().GetAllRecipes();
+            _contacts.DidNotReceive().CreateCompany(null, null);
+            _contacts.Received().CreatePerson("Bob", "Marley", "555-555-9999");
+
+        }
+
+        public void AddRecipe()
+        {
+            _input.GetNumber().Returns(6,0);
+            _input.GetNonEmptyString().Returns("Chicken");
+
+            _rolodex.DoStuff();
+
+            _input.Received(2).GetNumber();
+            _contacts.DidNotReceive().GetAllContacts();
+            _recipes.DidNotReceive().GetAllRecipes();
+            _contacts.DidNotReceive().CreateCompany(null, null);
+            _contacts.Received().CreatePerson(null,null,null);
+            _recipes.Received().Create("Chicken",RecipeType.Appetizers);
+            
+        }
+
+    }
+}
---
+++ AddressBook.Tests/_rolodex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook.Tests
+{
+    internal class _rolodex
+    {
+        public static implicit operator _rolodex(Rolodex v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
